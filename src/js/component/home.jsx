861b5cd40c36e91fc81cfd1c67314f17b9fa0bee
Celia Elias <148253357+celiaelias@users.noreply.github.com>
2024-02-15 21:53:01 +0000

    feat: todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	
+	const [ inputValue, setInputValue ] = useState("");
+	const [ todos, setTodos ] = useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<React.Fragment>
+			<head>
+				<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+      		</head>
+
+		<h1>Todos</h1>
+		<div className="container">
+			<ul>
+				<li>
+					<input 
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)} 
+						value={inputValue} 
+						onKeyUp={(e) => {
+							if (e.key === "Enter") { 
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
+						}}
+						placeholder="What do you need to do?"></input>
+				</li>
+				
+				{todos.map((item, index) => (
+					<li>
+						{item} {""} 
+						<FontAwesomeIcon icon={faTrashCan} className="trashcan" onClick={() => 
+								setTodos (
+									todos.filter((t, currentIndex) => index != currentIndex)
+								)
+							}/>
+					</li>
+				))}	
+			</ul>
+			<div>{todos.length} tasks</div>
 		</div>
+		
+		</React.Fragment>
 	);
 };
 
